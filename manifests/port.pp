14d52dbd6b178544f5712648932acf1633705c1c
Jochen Schalanda <j.schalanda@smarchive.de>
2013-01-10 14:03:05 +0100

    Fix puppet-lint warnings and errors

--- manifests/port.pp
+++ manifests/port.pp
@@ -39,20 +39,26 @@ define monitor::port (
   if ($tool =~ /monit/) {
   }
 
+  $tcp_check_command = $real_checksource ? {
+    local   => "check_nrpe!check_port_tcp!${target}!${port}",
+    default => "check_tcp!${port}",
+  }
+
+  $udp_check_command = $real_checksource ? {
+    local   => "check_nrpe!check_port_udp!${target}!${port}",
+    default => "check_udp!${port}",
+  }
+
+  $check_command = $protocol ? {
+    tcp => $tcp_check_command,
+    udp => $udp_check_command,
+  }
+
   if ($tool =~ /nagios/) {
     nagios::service { $name:
       ensure        => $ensure,
       template      => $real_template,
-      check_command => $protocol ? {
-        tcp => $real_checksource ? {
-          local   => "check_nrpe!check_port_tcp!${target}!${port}",
-          default => "check_tcp!${port}",
-        },
-        udp => $real_checksource ? {
-          local   => "check_nrpe!check_port_udp!${target}!${port}",
-          default => "check_udp!${port}",
-        },
-      }
+      check_command => $check_command,
     }
   }
 
@@ -60,28 +66,19 @@ define monitor::port (
     icinga::service { $name:
       ensure        => $ensure,
       template      => $real_template,
-      check_command => $protocol ? {
-        tcp => $real_checksource ? {
-          local   => "check_nrpe!check_port_tcp!${target}!${port}",
-          default => "check_tcp!${port}",
-        },
-        udp => $real_checksource ? {
-          local   => "check_nrpe!check_port_udp!${target}!${port}",
-          default => "check_udp!${port}",
-        },
-      }
+      check_command => $check_command,
     }
   }
 
+  $puppi_command = $protocol ? {
+    'tcp' => "check_tcp -H ${target} -p ${port}" ,
+    'udp' => "check_udp -H ${target} -p ${port}" ,
+  }
   if ($tool =~ /puppi/) {
     puppi::check { $name:
       enable   => $bool_enable,
       hostwide => 'yes',
-      command  => $protocol ? {
-        tcp => "check_tcp -H ${target} -p ${port}" ,
-        udp => "check_udp -H ${target} -p ${port}" ,
-      }
+      command  => $puppi_command,
     }
   }
-
 }
--- manifests/process.pp
+++ manifests/process.pp
@@ -38,18 +38,22 @@ define monitor::process (
     }
   }
 
+  $default_check_command = $argument ? {
+    undef   => "check_nrpe!check_process!${process}",
+    ''      => "check_nrpe!check_process!${process}",
+    default => "check_nrpe!check_processwitharg!${process}!${argument}",
+  }
+
+  $check_command = $process ? {
+    undef   => "check_nrpe!check_process!${name}",
+    default => $default_check_command,
+  }
+
   if ($tool =~ /nagios/) {
     nagios::service { $name:
       ensure        => $ensure,
       template      => $real_template,
-      check_command => $process ? {
-        undef   => "check_nrpe!check_process!${name}" ,
-        default => $argument ? {
-          undef   => "check_nrpe!check_process!${process}" ,
-          ''      => "check_nrpe!check_process!${process}" ,
-          default => "check_nrpe!check_processwitharg!${process}!${argument}" ,
-        }
-      }
+      check_command => $check_command,
     }
   }
 
@@ -57,30 +61,26 @@ define monitor::process (
     icinga::service { $name:
       ensure        => $ensure,
       template      => $real_template,
-      check_command => $process ? {
-        undef   => "check_nrpe!check_process!${name}" ,
-        default => $argument ? {
-          undef   => "check_nrpe!check_process!${process}" ,
-          ''      => "check_nrpe!check_process!${process}" ,
-          default => "check_nrpe!check_processwitharg!${process}!${argument}" ,
-        }
-      }
+      check_command => $check_command,
     }
+  }
+
+  $puppi_default_command = $argument ? {
+    undef   => "check_procs -c 1: -C ${process}",
+    ''      => "check_procs -c 1: -C ${process}",
+    default => "check_procs -c 1: -C ${process} -a ${argument}",
+  }
+
+  $puppi_command = $process ? {
+    undef   => "check_procs -c 1: -C ${name}",
+    default => $puppi_default_command,
   }
 
   if ($tool =~ /puppi/) {
     puppi::check { $name:
       enable   => $bool_enable,
       hostwide => 'yes',
-      command  => $process ? {
-        undef   => "check_procs -c 1: -C ${name}" ,
-        default => $argument ? {
-          undef   => "check_procs -c 1: -C ${process}" ,
-          ''      => "check_procs -c 1: -C ${process}" ,
-          default => "check_procs -c 1: -C ${process} -a ${argument}" ,
-        }
-      }
+      command  => $puppi_command,
     }
   }
-
 }
