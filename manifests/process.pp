1d7967554d6912bba0c6246c52e76e288558332f
Markus Langer <markus.langer@jumio.com>
2015-03-11 16:17:52 +0100

    added service_description to proc monitoring for icinga and nagios

--- manifests/process.pp
+++ manifests/process.pp
@@ -1,13 +1,14 @@
 define monitor::process (
   $tool,
-  $process      = $name,
-  $service      = undef,
-  $pidfile      = '',
-  $enable       = true,
-  $argument     = '',
-  $user         = '',
-  $template     = '',
-  $config_hash  = {}
+  $process             = $name,
+  $service             = undef,
+  $service_description = undef,
+  $pidfile             = '',
+  $enable              = true,
+  $argument            = '',
+  $user                = '',
+  $template            = '',
+  $config_hash         = {}
   ) {
 
   $bool_enable=any2bool($enable)
@@ -67,17 +68,19 @@ define monitor::process (
 
   if ($tool =~ /nagios/) {
     nagios::service { $name:
-      ensure        => $ensure,
-      template      => $real_template,
-      check_command => $check_command,
+      ensure              => $ensure,
+      service_description => $service_description,
+      template            => $real_template,
+      check_command       => $check_command,
     }
   }
 
   if ($tool =~ /icinga/) {
     icinga::service { $name:
-      ensure        => $ensure,
-      template      => $real_template,
-      check_command => $check_command,
+      ensure              => $ensure,
+      service_description => $service_description,
+      template            => $real_template,
+      check_command       => $check_command,
     }
   }
 
